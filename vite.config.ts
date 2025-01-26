c5932763ec5b6c3e8cabb82e73349ef764de910a
Phudit Chuengjaroen <phudit.nut@gmail.com>
2025-01-26 14:02:17 +0700

    chore: test deployment script

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,34 @@
-import path from "path";
+import path from 'path';
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import type { UserConfig } from 'vite';
+
+import { name } from './package.json';
 
 const ReactCompilerConfig = {
-  target: "19", // '17' | '18' | '19'
+  target: '19', // '17' | '18' | '19'
 };
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite(),
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+export default defineConfig(({ mode }) => {
+  // TODO: do we need env from node when build if not skip this.
+  const _env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: name,
+    plugins: [
+      TanStackRouterVite(),
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
-  },
+  } satisfies UserConfig;
 });
